a60fd9cd733a9211ad53737435b63c371b788a9c
tkanchanawanchai6403 <tkanchanawanchai6403@an098046-ct103v.nvstu.edu>
2015-01-31 11:39:29 -0500

    add message //version4

--- Overloading/src/Gps.java
+++ Overloading/src/Gps.java
@@ -1,6 +1,7 @@
 // change v1 do not commit
 // change v2 commit - initial version //master & head in the same location
 // change v3 commit
+// change v4 commit
 public class Gps {
 	
 	public String getDirection() {
